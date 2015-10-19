0f703702e416f0cb504d5af571d02f4697069f0a
mloayzat <mloayzat@chicagobooth.edu>
2015-10-19 14:41:54 -0500

    test

--- rps.rb
+++ rps.rb
@@ -1,1 +1,22 @@
-# Rock, Paper, Scissors
+ # Rock, Paper, Scissors
+
+ puts "Your move: rock, paper, or scissors?"
+
+ move = gets.chomp 
+ puts "you chose #{move}"
+
+vschoice = ["rock", "paper", "scissors"].sample 
+
+puts "Computer chose #{vschoice}"
+
+
+if move == vschoice	
+	 puts "It's a tie!"
+elsif move == "rock" && vschoice == "paper" || move == "scissors" && vschoice == "rock" || move == "paper" && vschoice == "scissors" 
+	puts "You lose!"
+elsif move == "scissors" && vschoice == "paper" || move == "paper" && vschoice == "rock"  || move == "rock" && vschoice == "scissors" 
+	puts "You win!"
+
+else puts "That doesn't seem to be a valid move, what were you thinking, it was supposed to be a simple game."
+
+end
